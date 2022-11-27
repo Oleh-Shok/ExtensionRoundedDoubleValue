ddeb922933cc95f8c0d0a005866ab8e387fe414c
olegshok <pastushok91@gmail.com>
2022-11-27 22:36:52 +0200

    Add Program.cs + solution + project file for ExtensionRoundedDoubleValue

---
+++ RoundDouble/Program.cs
@@ -0,0 +1,12 @@
+﻿//int i = 2.4.Round(); i = 2
+//int j = 2.5.Round(); j = 3
+
+Console.WriteLine(2.4.Round());
+Console.WriteLine(2.5.Round());
+
+static class ExtensionDouble
+{
+
+    public static int Round(this double RoundValue) => Convert.ToInt32(Math.Round(RoundValue, MidpointRounding.AwayFromZero));
+}
+
